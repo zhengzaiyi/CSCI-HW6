a40e1c5a7f6c14ff5bffc1e1447c6531dbd1b7df
zhengzaiyi <44119778+zhengzaiyi@users.noreply.github.com>
2022-10-09 07:51:57 -0700

    1

--- application.py
+++ application.py
@@ -2,23 +2,23 @@ from flask import Flask, jsonify, request
 from yelpapi import YelpAPI
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app, resources=r'/*')
+application = Flask(__name__)
+CORS(application, resources=r'/*')
 yelp_api_key = 'vzyregVWvR0tirtuW4dYVHHsipTo63Xz-Z3JPdi0oGWrYRallgGeIcmua0PlmU09xApul0Eo8HSJuR1itXtxgq5J5fQ47KZrqC_kj-Y6nl8ebyhXZ6v0Sx_NtsE-Y3Yx'
 yelp_api = YelpAPI(yelp_api_key)
-@app.route('/')
+@application.route('/')
 def homepage():
-    return app.send_static_file('index.html')
+    return application.send_static_file('index.html')
 
-@app.route('/style.css')
+@application.route('/style.css')
 def styleFile():
-    return app.send_static_file('style.css')
+    return application.send_static_file('style.css')
 
-@app.route('/frontend.js')
+@application.route('/frontend.js')
 def jsFile():
-    return app.send_static_file('frontend.js')
+    return application.send_static_file('frontend.js')
 
-@app.route('/request_search', methods=['POST', 'GET'])
+@application.route('/request_search', methods=['POST', 'GET'])
 def search():
     data = dict(request.args)
     if len(data['radius']) > 0:
@@ -28,7 +28,7 @@ def search():
     result = yelp_api.search_query(**data)
     return jsonify(result)
 
-@app.route('/request_details', methods=['POST', 'GET'])
+@application.route('/request_details', methods=['POST', 'GET'])
 def details():
     data = dict(request.args)
     result = yelp_api.business_query(data['id'])
@@ -36,4 +36,4 @@ def details():
     
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+    application.run(host='127.0.0.1')
